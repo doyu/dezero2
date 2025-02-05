b07a465f6c2822fea24046d6209774ec2c90a618
Hiroshi Doyu <hiroshi.doyu@ninjalabo.ai>
2025-02-05 18:40:38 +0200

    step09: Var == None | np.ndarry
    Only allowed

--- dezero2/core.py
+++ dezero2/core.py
@@ -11,6 +11,9 @@ import numpy as np
 # %% ../nbs/00_core.ipynb 4
 class Variable:
     def __init__(self, data):
+        if data is not None: # Why allowing "None" here?
+            if not isinstance(data, np.ndarray):
+                raise TypeError("{} is not supported".format(type(data)))
         self.data = data
         self.grad = None
         self.creator = None
@@ -35,6 +38,7 @@ class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
+        y = np.array(y) if np.isscalar(y) else y # for numpy spec
         output = Variable(y)
         output.set_creator(self)
         self.input = input
@@ -76,8 +80,8 @@ def exp(x):
 
 # %% ../nbs/00_core.ipynb 12
 def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
+    x0 = Variable(np.array(x.data - eps))
+    x1 = Variable(np.array(x.data + eps))
     y0 = f(x0)
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
