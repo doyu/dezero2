2bb9dc2d6b5110903c0d5f97c4494f7666949d35
Hiroshi Doyu <hiroshi.doyu@ninjalabo.ai>
2025-02-05 10:13:23 +0200

    step01: add Variable class

--- dezero2/_modidx.py
+++ dezero2/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://doyu.github.io',
                 'git_url': 'https://github.com/doyu/dezero2',
                 'lib_path': 'dezero2'},
-  'syms': {'dezero2.core': {'dezero2.core.foo': ('core.html#foo', 'dezero2/core.py')}}}
+  'syms': { 'dezero2.core': { 'dezero2.core.Variable': ('core.html#variable', 'dezero2/core.py'),
+                              'dezero2.core.Variable.__init__': ('core.html#variable.__init__', 'dezero2/core.py')}}}
