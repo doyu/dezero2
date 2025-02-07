252997947541d73f3b8645d96f324cfaaa8ffff9
Hiroshi Doyu <hiroshi.doyu@ninjalabo.ai>
2025-02-07 11:51:15 +0200

    step18: unnecessary intermediate grads

--- dezero2/core.py
+++ dezero2/core.py
@@ -24,7 +24,7 @@ class Variable:
         self.creator = func
         self.generation = func.generation + 1
 
-    def backward(self):
+    def backward(self, retain_grad=False):
         if self.grad == None:
             self.grad = np.ones_like(self.data)
 
@@ -52,6 +52,10 @@ class Variable:
                 x.grad = gx if x.grad==None else x.grad+gx
                 if x.creator is not None:
                     add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
 
     def cleargrad(self):
         self.grad = None
